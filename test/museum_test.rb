409bc11361ae44d811265163d02621e9d6de3e18
Kris Litman <duguy8@gmail.com>
2021-01-07 11:32:28 -0500

    Test made to check that the museum has no patrons by default, and that they can be added.

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -46,10 +46,35 @@ class MuseumTest < Minitest::Test
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
 
-    expected_patron1 = [dead_sea_scrolls, gems_and_minerals]
+    expected_patron1 = [gems_and_minerals, dead_sea_scrolls]
     assert_equal expected_patron1, dmns.recommend_exhibits(patron_1)
 
     expected_patron2 = [imax]
     assert_equal expected_patron2, dmns.recommend_exhibits(patron_2)
+  end
+
+  def test_museum_can_admit_patrons
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal ([]), dmns.patrons
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    expected = [patron_1, patron_2, patron_3]
+    assert_equal expected, dmns.patrons
   end
 end
