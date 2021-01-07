bfb7f0256f9c2f027033e77e56135156688b930b
Kris Litman <duguy8@gmail.com>
2021-01-07 11:14:59 -0500

    Test made to check if patron can add interests.

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -12,7 +12,15 @@ class PatronTest < Minitest::Test
   def test_patron_attributes
     patron_1 = Patron.new("Bob", 20)
     assert_equal "Bob", patron_1.name
-    assert_equal 20, patron.spending_money
+    assert_equal 20, patron_1.spending_money
     assert_equal ([]), patron_1.interests
+  end
+
+  def test_patron_can_add_interests
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    expected = ["Dead Sea Scrolls", "Gems and Minerals"]
+    assert_equal expected, patron_1.interests
   end
 end
