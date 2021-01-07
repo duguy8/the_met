e20610caba0477753d1f71e925d3c08504a4385d
Kris Litman <duguy8@gmail.com>
2021-01-07 11:07:57 -0500

    Create tests for attributes for Exhibit

---
+++ lib/exhibit.rb
@@ -0,0 +1,6 @@
+class Exhibit
+
+  def initialize(input)
+    @input = input
+  end
+end
--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -6,5 +6,11 @@ class ExhibitTest < Minitest::Test
   def test_it_exists
     exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
     assert_instance_of Exhibit, exhibit
-    end
   end
+
+  def test_attributes
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
+end
